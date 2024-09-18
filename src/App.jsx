9ea055371f2e803b47cea39b85b250a2baa8960f
Sneha Manded <snehamanded@gmail.com>
2024-09-18 10:16:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Model from "./components/Model";
+import Highlights from "./components/Highlights";
+
+const App=() =>{
+  return (
+  <main className="bg-black">
+    <Navbar/>
+    <Hero/>    
+    <Highlights/>
+    <Model/>
+    </main>
+  )
+}
+export default App
